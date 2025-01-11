022117ad5702aa3c999627371b98cdb1210a955c
Evy-747 <jeronoevy@gmail.com>
2025-01-11 20:08:42 +0300

    Add file

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here.from django.contrib import admin
+from .models import Book
+
+# Registering the Book model
+admin.site.register(Book)
+
